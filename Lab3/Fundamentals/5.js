1f8b502ba62a8d9aadda3a4906fc079dc94e2d9c
Aidar Zhaxylykuly <122877465+AidarZhaxylykuly@users.noreply.github.com>
2024-02-14 21:51:59 +0600

    Add files via upload
    14.08.2024

---
+++ Lab3/Fundamentals/5.js
@@ -0,0 +1,52 @@
+let message = 'Hello!'; // define the variable and assign the value
+
+alert(message); 
+
+let user = 'John', age = 25;
+
+var mes = 'Hello';
+// OldSchool
+
+message = 'World!'; // value changed
+
+alert(message);
+
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
+
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+
+alert($ + _); // 3
+
+
+let имя = '...';
+let 我 = '...';
+
+// note: no "use strict" in this example
+
+num = 5; // the variable "num" is created if it didn't exist(OldSchool)
+
+alert(num); // 5
+
+const myBirthday = '18.04.1982';
+
+//myBirthday = '01.01.2001'; // error, can't reassign the constant!
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+
+
