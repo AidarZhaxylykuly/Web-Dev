87cda916aba96b3799003477fa32b0b4e06e0d10
Aidar <a_zhaxylykuly@kbtu.kz>
2024-02-14 21:46:42 +0600

    Lab 3

---
+++ Lab3/Fundamentals/3.js
@@ -0,0 +1,8 @@
+// This comment occupies a line of its own
+alert('Hello');
+
+alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+// */
